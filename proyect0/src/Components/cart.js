e0cb66a79e33d77c1332d45b3071cc34251f678b
Alex Trujillo Vera <aleztrujillo91@hotmail.com>
2023-11-06 23:09:59 -0500

    V 1.4.3

--- proyect0/src/Components/cart.js
+++ proyect0/src/Components/cart.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function Producto(props){
     return(
         <li class="list-group-item d-flex justify-content-beetween align-item-start">
@@ -5,19 +7,33 @@ function Producto(props){
                 <div class ="fw-bold">{props.name}</div>
                 {props.description}
             </div>
-            <spam class ="badge bg-primary rounded-pill">{props.price}</spam>
+            <span class ="badge bg-primary rounded-pill">{props.price}</span>
         </li>
     );
 }
 
-export default function Cart(){
+export default function Cart(props){
+    const[total, setTotal] = useState(0);
     return(
-        <div className="col-4">
+        <div className="col-4 mh-100" style = {{maxHeight:300 + "px !important"}}>
         <h2 className="d-flex justify-content-center">carrito compras</h2>
-        <ol class="list-group list-group-numbered">
-        <Producto name ="Hosting" description ="venta de dominio y hosting" price ={450}>
-        </Producto> 
+        <ol class="list-group list-group-numbered" id="list">
+        {props.selectedItems.map(item) => {
+            setTotal(total + item.price)
+                return (
+                    <Producto 
+                    name={item.name} 
+                    description={item.description}
+                    price={item.precio}
+                    ></Producto>
+                );
+            })} 
         </ol>
+            <div className="d-flex justify-content-evenly col-12 mt-2">
+                <button type="button" class="btn btn-outline-dark">
+                    total:{total}</button>
+                <button type="button" class="btn btn-sucess">PAGAR</button>
+            </div>
          </div>
     );
 }
--- proyect0/src/Components/products.js
+++ proyect0/src/Components/products.js
@@ -1,24 +1,59 @@
 function ProductElement(props){
+    
+    function addToCart(){
+    props.add((arrayViejito)=>[...arrayViejito, {name:props.name, price:props.price,description:props.description}]);
+            
+    }
     return(
-        <div class="card col-3">
+        <div class="card col-3 m-3">
             <img src="..." class ="card-img-top" alt="..."/>
             <div class="card-body">
             <h5 class="card-title">{props.name}</h5>
                <P class="card-text">{props.description}</P>
+               <div className="d-flex justify-content-around col-12">
                <button class ="btn btn-primary">{props.price}</button>
-            </div>
+               <button type ="button" class="btn btn-success" onClick={addToCart}>
+                Comprar
+                </button>
+                </div>
+                </div>
         </div>
 
     );
 }
 
-export default function ProductList(){
+export default function ProductList(props){
+    let products = [{
+        name:"dominios y hosting",
+        price: 400,
+        description: "adquiere el servicio ya",
+    },
+    {
+        name:"creacion de pagina web",
+        price: 200,
+        description: "tu pagina en tu sitio",
+    },
+    {
+        name:"posicionamiento",
+        price: 1100,
+        description: "rankea tu pagina en google",
+    },
+
+];
     return(
         <div className="col-8 f-flex-wrap justify-content-around">
             <h3 className="col-12 d-flex justify-content-center">Catalogo</h3>
-            <ProductElement name="Hosting" description="adquiere tu servicio ya" price={450}></ProductElement>
-            <ProductElement></ProductElement>
-            <ProductElement></ProductElement>
+            {products.map(products) => {
+                return (
+                <ProductElement 
+                name={products.name} 
+                price={products.price} 
+                description={products.description}
+                add={props.setSelectItem}
+                >
+                </ProductElement>
+                );                
+            })}
         </div>
     );
 }
